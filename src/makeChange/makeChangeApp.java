e70586655d22d9033165da3ca38acea15cd0f4b6
darinthompson <darint07@gmail.com>
2020-03-21 13:04:59 -0600

    added input validation and added better formatting for output

--- src/makeChange/makeChangeApp.java
+++ src/makeChange/makeChangeApp.java
@@ -2,24 +2,31 @@ package makeChange;
 
 import java.util.Scanner;
 
+/*
+ * I have set up a loop to be able to test all the cases without having to keep re-running the app.
+ * just enter 0 for item amount to stop the program
+ */
 public class makeChangeApp {
 
 	static Scanner sc = new Scanner(System.in);
-
+	static String ERROR = "\u001B[31m";
 	public static void main(String[] args) {
-		while (true) { // <-- added this while loop to run the test cases without having to re-run the
-						// program
+
+		while (true) {
 			int tens = 0, fives = 0, ones = 0, qts = 0, dms = 0, nks = 0, pns = 0;
 			int[] monies = new int[7];
 			double amountLeft;
-			System.out.print("Price of item: ");
-			double price = sc.nextDouble();
+			double price = ValidateInput("Item Amount: ");
 
-			System.out.print("Amount tendered: ");
-			double amount = sc.nextDouble();
+			if (price == 0) {
+				System.exit(0);
+			}
+
+			double amount = ValidateInput("Amount tendered: ");
 			amount += 0.0005;
 			if (amount < price) {
-				System.out.println("insufficient funds...");
+				printSpacing();
+				System.out.println("insufficient funds...\n");
 			} else if (amount == price) {
 				System.out.println("Nice doing business with you! Come again soon!");
 			} else {
@@ -28,7 +35,6 @@ public class makeChangeApp {
 					tens++;
 					monies[0] = tens;
 					amountLeft = amountLeft - 10.0;
-					sc.nextLine();
 				}
 				while (amountLeft >= 5.0) {
 					fives++;
@@ -61,39 +67,57 @@ public class makeChangeApp {
 					amountLeft = amountLeft - .01;
 				}
 
+				System.out.println("");
 				System.out.println("Total Change: $" + String.format("%.2f", (amount - price)));
+				printSpacing();
 				for (int i = 0; i < monies.length; i++) {
 					if (monies[i] != 0) {
 						switch (i) {
 						case 0:
-							System.out.print("Tens: " + monies[i] + " ");
+							System.out.print("Tens: " + monies[i] + " | ");
 							break;
 						case 1:
-							System.out.print("Fives: " + monies[i] + " ");
+							System.out.print("Fives: " + monies[i] + " | ");
 							break;
 						case 2:
-							System.out.print("Ones: " + monies[i] + " ");
+							System.out.print("Ones: " + monies[i] + " | ");
 							break;
 						case 3:
-							System.out.print("Quarters: " + monies[i] + " ");
+							System.out.print("Quarters: " + monies[i] + " | ");
 							break;
 						case 4:
-							System.out.print("Dimes: " + monies[i] + " ");
+							System.out.print("Dimes: " + monies[i] + " | ");
 							break;
 						case 5:
-							System.out.print("Nickels: " + monies[i] + " ");
+							System.out.print("Nickels: " + monies[i] + " | ");
 							break;
 						case 6:
-							System.out.print("Pennies: " + monies[i] + " ");
+							System.out.print("Pennies: " + monies[i] + " \n\n");
 						}
 					}
 				}
 			}
-			
-			System.out.println();
-			System.out.println("Press ENTER to continue... (Press esc to end program)");
-			sc.nextLine();
 		}
+	}
 
+	public static double ValidateInput(String prompt) {
+		double userNum;
+		while (true) {
+			System.out.print(prompt);
+			try {
+				userNum = sc.nextDouble();
+				break;
+			} catch (Exception e) {
+				System.out.println(ERROR + "Invalid amount.");
+				printSpacing();
+				System.out.println();
+				sc.nextLine();
+			}
+		}
+		return userNum;
+	}
+	
+	static void printSpacing() {
+		System.out.println("*************************************************");
 	}
 }
