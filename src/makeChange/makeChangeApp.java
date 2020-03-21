8267c616219878f5f242a2f2fc7d01446c187f16
darinthompson <darint07@gmail.com>
2020-03-21 10:47:58 -0600

    fixed output to only print monies with values

--- src/makeChange/makeChangeApp.java
+++ src/makeChange/makeChangeApp.java
@@ -7,8 +7,10 @@ public class makeChangeApp {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		while (true) {
-			double tens = 0, fives = 0, ones = 0, qts = 0, dms = 0, nks = 0, pns = 0;
+		while (true) { // <-- added this while loop to run the test cases without having to re-run the
+						// program
+			int tens = 0, fives = 0, ones = 0, qts = 0, dms = 0, nks = 0, pns = 0;
+			int[] monies = new int[7];
 			double amountLeft;
 			System.out.print("Price of item: ");
 			double price = sc.nextDouble();
@@ -24,43 +26,72 @@ public class makeChangeApp {
 				amountLeft = amount - price;
 				while (amountLeft >= 10.0) {
 					tens++;
+					monies[0] = tens;
 					amountLeft = amountLeft - 10.0;
-					System.out.println(amountLeft);
 					sc.nextLine();
 				}
 				while (amountLeft >= 5.0) {
 					fives++;
+					monies[1] = fives;
 					amountLeft = amountLeft - 5.0;
-					System.out.println(amountLeft);
 				}
 				while (amountLeft >= 1.0) {
 					ones++;
+					monies[2] = ones;
 					amountLeft = amountLeft - 1.0;
-					System.out.println(amountLeft);
 				}
 				while (amountLeft >= .25) {
 					qts++;
+					monies[3] = qts;
 					amountLeft = amountLeft - .25;
-					System.out.println(amountLeft);
 				}
 				while (amountLeft >= .10) {
 					dms++;
+					monies[4] = dms;
 					amountLeft = amountLeft - .10;
 				}
 				while (amountLeft >= .05) {
 					nks++;
+					monies[5] = nks;
 					amountLeft = amountLeft - .05;
-					System.out.println(amountLeft);
 				}
 				while (amountLeft >= .01) {
 					pns++;
+					monies[6] = pns;
 					amountLeft = amountLeft - .01;
-					System.out.println(amountLeft);
 				}
+
 				System.out.println("Total Change: $" + String.format("%.2f", (amount - price)));
-				System.out.println("Denomination: Tens - " + (int)tens + ", Fives - " + (int)fives + ", Ones - " + (int)ones + ", Quarters - " + (int)qts
-						+ ", Dimes - " + (int)dms + ", Nickles - " + (int)nks + ", Pennies - " + (int)pns);
+				for (int i = 0; i < monies.length; i++) {
+					if (monies[i] != 0) {
+						switch (i) {
+						case 0:
+							System.out.print("Tens: " + monies[i] + " ");
+							break;
+						case 1:
+							System.out.print("Fives: " + monies[i] + " ");
+							break;
+						case 2:
+							System.out.print("Ones: " + monies[i] + " ");
+							break;
+						case 3:
+							System.out.print("Quarters: " + monies[i] + " ");
+							break;
+						case 4:
+							System.out.print("Dimes: " + monies[i] + " ");
+							break;
+						case 5:
+							System.out.print("Nickels: " + monies[i] + " ");
+							break;
+						case 6:
+							System.out.print("Pennies: " + monies[i] + " ");
+						}
+					}
+				}
 			}
+			
+			System.out.println();
+			System.out.println("Press ENTER to continue... (Press esc to end program)");
 			sc.nextLine();
 		}
 
